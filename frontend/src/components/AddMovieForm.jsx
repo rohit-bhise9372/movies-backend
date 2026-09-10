b87d70aeba8ba0bdaa5288d9aa94bfd7c43f3c64
Rohit Bhise <rohitbhise9372@gmail.com>
2026-09-10 16:19:45 +0530

    Update project

--- frontend/src/components/AddMovieForm.jsx
+++ frontend/src/components/AddMovieForm.jsx
@@ -1,76 +1,188 @@
+
 import React, { useState } from "react";
 
 const AddMovieForm = () => {
-    const [formData, setFormData] = useState({
-        title: "",
-        releaseYear: "",
-        genre: "",
-        director: "",
-        actors: "",
-        language: "",
-        country: "",
-        rating: "",
-        plot: "",
-        awards: "",
-        posterUrl: "",
-        trailerUrl: "",
-    });
+  const [formData, setFormData] = useState({
+    title: "",
+    releaseYear: "",
+    genre: "",
+    director: "",
+    actors: "",
+    language: "",
+    country: "",
+    rating: "",
+    plot: "",
+    awards: "",
+    posterUrl: "",
+    trailerUrl: "",
+  });
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-        setFormData((prevState) => ({
-            ...prevState,
-            [name]:
-                name === "releaseYear" || name === "rating"
-                    ? parseInt(value)
-                    : value,
-        }));
-    };
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]:
+        name === "releaseYear" || name === "rating"
+          ? parseInt(value)
+          : value,
+    }));
+  };
 
-    return (
-        <div>
-            <h2>Add New Movie</h2>
+  return (
+    <div>
+      <h2>Add New Movie</h2>
 
-            <form>
-                <label>Title:</label>
-                <br />
-                <input
-                    type="text"
-                    name="title"
-                    value={formData.title}
-                    onChange={handleChange}
-                />
+      <form>
+        <label>Title:</label>
+        <br />
+        <input
+          type="text"
+          name="title"
+          value={formData.title}
+          onChange={handleChange}
+        />
 
-                <br />
-                <br />
+        <br />
+        <br />
 
-                <label>Release Year:</label>
-                <br />
-                <input
-                    type="number"
-                    name="releaseYear"
-                    value={formData.releaseYear}
-                    onChange={handleChange}
-                />
+        <label>Release Year:</label>
+        <br />
+        <input
+          type="number"
+          name="releaseYear"
+          value={formData.releaseYear}
+          onChange={handleChange}
+        />
 
-                <br />
-                <br />
+        <br />
+        <br />
 
-                <label>Genre:</label>
-                <br />
-                <input
-                    type="text"
-                    name="genre"
-                    value={formData.genre}
-                    onChange={handleChange}
-                />
+        <label>Genre:</label>
+        <br />
+        <input
+          type="text"
+          name="genre"
+          value={formData.genre}
+          onChange={handleChange}
+        />
 
-                <br />
-                <br />
-            </form>
-        </div>
-    );
+        <br />
+        <br />
+
+        <label>Director:</label>
+        <br />
+        <input
+          type="text"
+          name="director"
+          value={formData.director}
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <label>Actors:</label>
+        <br />
+        <input
+          type="text"
+          name="actors"
+          value={formData.actors}
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <label>Language:</label>
+        <br />
+        <input
+          type="text"
+          name="language"
+          value={formData.language}
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <label>Country:</label>
+        <br />
+        <input
+          type="text"
+          name="country"
+          value={formData.country}
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <label>Rating:</label>
+        <br />
+        <input
+          type="number"
+          name="rating"
+          value={formData.rating}
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <label>Plot:</label>
+        <br />
+        <input
+          type="text"
+          name="plot"
+          value={formData.plot}
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <label>Awards:</label>
+        <br />
+        <input
+          type="text"
+          name="awards"
+          value={formData.awards}
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <label>Poster URL:</label>
+        <br />
+        <input
+          type="text"
+          name="posterUrl"
+          value={formData.posterUrl}
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <label>Trailer URL:</label>
+        <br />
+        <input
+          type="text"
+          name="trailerUrl"
+          value={formData.trailerUrl}
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default AddMovieForm;
+
