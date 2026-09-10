63bcddcb8d7cd554ab08841c038a9bdf9c35b54d
Rohit Bhise <rohitbhise9372@gmail.com>
2026-09-10 16:13:54 +0530

    Update project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,14 @@
 import "./App.css";
 import MovieByTitle from "./components/MovieByTitle";
-import Movies from "./components/Movies"
-import AddMovieForm from "./components/AddMovieForm"
+import Movies from "./components/Movies";
+import AddMovieForm from "./components/AddMovieForm";
 
 export default function App() {
   return (
     <main>
-      <AddMovieForm/>
+      <AddMovieForm />
       <Movies />
       <MovieByTitle title="Gully Boy" />
-
-  </main>
+    </main>
   );
 }
-
-
-
-
