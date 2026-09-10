2d7c88b2aefff0b7dcdf9b3c792d34a59093d1e6
Rohit Bhise <rohitbhise9372@gmail.com>
2026-09-10 15:58:58 +0530

    Update project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import MovieByTitle from "./components/MovieByTitle";
 import Movies from "./components/Movies"
+import AddMovieForm from "./components/AddMovieForm"
 
 export default function App() {
   return (
     <main>
       <Movies />
-      <MovieByTitle title = "Gully Boy"/>
+      <MovieByTitle title="Gully Boy" />
+      <AddMovieForm/>
   </main>
   );
 }
---
+++ frontend/src/components/AddMovieForm.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+
+const AddMovieForm = () => {
+    const [formData, setFormData] = useState({
+        title: "",
+        releaseYear: "",
+        genre: "",
+        director: "",
+        actors: "",
+        language: "",
+        country: "",
+        rating: "",
+        plot: "",
+        awards: "",
+        posterUrl: "",
+        trailerUrl: "",
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevState) => ({
+            ...prevState,
+            [name]:
+                name === "releaseYear" || name === "rating" ? parseInt(value) : value,
+        }));
+    };
+
+    return (
+        <div>
+            <h2>Add New Movie</h2>
+            <form>
+                <label>Title:</label>
+                <br />
+                <input
+                    type="text"
+                    name="title"
+                    value={formData.title}
+                    onChange={handleChange}
+                />
+                <br />
+                <br />
+                <label>Release Year:</label>
+                <br />
+                <input
+                    type="number"
+                    name="releaseYear"
+                    value={formData.releaseYear}
+                    onChange={handleChange}
+                />
+                <br />
+                <br />
+                <label>Genre:</label>
+                <br />
+                <input
+                    type="text"
+                    name="genre"
+                    value={formData.genre}
+                    onChange={handleChange}
+                />
+                <br />
+                <br />
+            </form>
+        </div>)
+}
