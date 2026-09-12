a310a38c7dffcf5ae0f73c9e3881867e43651644
Rohit Bhise <rohitbhise9372@gmail.com>
2026-09-12 15:03:15 +0530

    Update movie frontend styling and component

--- frontend/src/components/Movies.jsx
+++ frontend/src/components/Movies.jsx
@@ -1,9 +1,32 @@
+import  {useState}  from "react";
 import useFetch from "../useFetch";
 
+
 const Movies = () => {
+  const [successMessage, setSuccessMessage] = useState();
     const { data, loading, error } = useFetch("https://movies-backend-xi.vercel.app/movies")
     
-    console.log(data);
+  // console.log(data);
+  
+  const handleDelete = async (movieId) => {
+    try {
+      const response = await fetch(`https://movies-backend-xi.vercel.app/movies/${movieId}`,
+        { method: "DELETE" }
+      );
+
+      if (!response.ok) {
+        throw "Failed to delete movie."
+      }
+
+      const data = await response.json()
+      if (data) {
+        setSuccessMessage("Movie deleted successfully");
+        window.location.reload();
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
 return (
   <div>
@@ -11,9 +34,13 @@ return (
     {data?.error && <p>{data?.error}</p>}
     <ul>
       {data?.map((movie) => (
-        <li key={movie._id}>{movie.title}</li>
+        <li key={movie._id}>{movie.title}
+          <button onClick={() => handleDelete(movie._id)}>Delete</button>
+        </li>
+        
       ))}
     </ul>
+    <p>{ successMessage}</p>
   </div>
 );
 };
